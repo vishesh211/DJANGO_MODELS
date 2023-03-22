4af3e2c57192551f343391b5aabb6f13ca90a7ff
vishesh211 <visheshtiwari211@gmail.com>
2023-03-22 17:15:44 +0530

    created model class Patients

--- my_site/office/models.py
+++ my_site/office/models.py
@@ -1,3 +1,10 @@
+from operator import mod
+from pyexpat import model
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
